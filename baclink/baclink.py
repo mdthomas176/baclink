eb273a886230dd49842943ec77a91b32856c1342
Michael Thomas <mdthomas176@gmail.com>
2026-02-24 11:33:34 -0500

    add bac0

---
+++ baclink/baclink.py
@@ -0,0 +1,42 @@
+import time
+import BAC0
+
+from BAC0.core.devices.local.factory import (
+    analog_input,
+    character_string,
+)
+
+bacnet = BAC0.lite(
+    ip="192.168.200.68/24",
+    deviceId=1234,
+    localObjName="MyDevice",
+)
+
+# Define objects,
+analog_input(
+    name="TW1",
+    description="Water temperature 1",
+    properties={"units": "degreesCelsius"},
+    presentValue=0.0,
+)
+
+character_string(
+    name="Application_Status",
+    description="Health",
+    presentValue="Normal",
+)
+
+# Register all defined objects with the BACnet application,
+analog_input.add_objects_to_application(bacnet)
+
+def get_plc_value():
+    # TODO: replace with your PLC read
+    return 22.3
+
+while True:
+    try:
+        bacnet["TW1"].presentValue = float(get_plc_value())
+        bacnet["Application_Status"].presentValue = "Normal"
+    except Exception as e:
+        bacnet["Application_Status"].presentValue = str(e)[:50]
+    time.sleep(5)
